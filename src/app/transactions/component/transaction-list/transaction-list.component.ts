294786ca9284373581f4be7a133186955394fd85
queency-koh <queencykoh@gmail.com>
2021-06-16 07:22:00 +0800

    Added Angular Material Side Navigation for Transaction Detail

--- src/app/transactions/component/transaction-list/transaction-list.component.ts
+++ src/app/transactions/component/transaction-list/transaction-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Transaction } from '../../shared/models/transaction.model';
 
 @Component({
@@ -6,13 +8,19 @@ import { Transaction } from '../../shared/models/transaction.model';
   templateUrl: './transaction-list.component.html',
   styleUrls: ['./transaction-list.component.scss']
 })
-export class TransactionListComponent implements OnInit {
+export class TransactionListComponent{
+
+  @ViewChild('matDrawer', {static: true}) matDrawer!: MatDrawer;
 
   @Input()
   data: Transaction[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {}
 
-  ngOnInit(): void {
+  createTransaction(type: 'expense' | 'income'): void
+  {
+    // Go to transaction
+    this.router.navigate(['/', type], {relativeTo: this.route});
   }
 }
