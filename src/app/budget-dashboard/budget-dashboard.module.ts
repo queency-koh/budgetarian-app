6f877ae76fe825c75249012c4eca0b660b037433
queency-koh <queencykoh@gmail.com>
2021-07-09 21:09:01 +0800

    Added Budget Status Card Component

--- src/app/budget-dashboard/budget-dashboard.module.ts
+++ src/app/budget-dashboard/budget-dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BudgetDashboardComponent } from './budget-dashboard/budget-dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BudgetStatusCardComponent } from './budget-status-card/budget-status-card.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     BudgetDashboardComponent,
+    BudgetStatusCardComponent
   ],
   imports: [
     RouterModule.forChild(routes),
---
+++ src/app/budget-dashboard/budget-status-card/budget-status-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-status-card',
+  templateUrl: './budget-status-card.component.html',
+  styleUrls: ['./budget-status-card.component.scss']
+})
+export class BudgetStatusCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
