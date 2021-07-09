a1078d8ea8704695de252b60dad2dda68a9b874d
queency-koh <queencykoh@gmail.com>
2021-07-09 21:06:30 +0800

    Lazy Load Budget Dashboard Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'dashboard',
+    loadChildren: () => import('./budget-dashboard/budget-dashboard.module').then(m => m.BudgetDashboardModule)
+  },
+  {
     path: 'transactions',
     loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule)
   }
