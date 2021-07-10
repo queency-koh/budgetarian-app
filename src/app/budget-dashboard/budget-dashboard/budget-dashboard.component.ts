1f2f32e7cb0fdd8e0a42a3aa35bf66e2878a015a
queency-koh <queencykoh@gmail.com>
2021-07-10 11:43:14 +0800

    Calculate Expenses and Savings

--- src/app/budget-dashboard/budget-dashboard/budget-dashboard.component.ts
+++ src/app/budget-dashboard/budget-dashboard/budget-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-budget-dashboard',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-dashboard.component.scss']
 })
 export class BudgetDashboardComponent implements OnInit {
+  savings: number = 0;
+  income: number = 115000;
+  expenses$!: Observable<number>;
+  transactions: Transaction[] = new Array<Transaction>();
 
-  constructor() { }
+  constructor(private transactionService: TransactionService) { }
 
   ngOnInit(): void {
-  }
 
+    // Get All Transactions
+    const transactions$ = this.transactionService.get();
+
+    // Calculate Total Expense
+    this.expenses$ = transactions$
+      .pipe(
+        map(transactions => transactions
+          .reduce((total, transaction) => total + (+transaction.amount), 0)));
+
+    // Calculate Total Savings
+    this.expenses$.subscribe((expenses) => this.savings = this.income - expenses);
+  }
 }
