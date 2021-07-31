5ffefe15bb95be5173caea3eed7fe73b5517c88b
queency-koh <queencykoh@gmail.com>
2021-07-31 14:50:19 +0800

    Added Auth Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from './auth/auth.module';
 import { BudgetDashboardModule } from './budget-dashboard/budget-dashboard.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     BudgetDashboardModule,
     TransactionsModule,
     BrowserAnimationsModule
